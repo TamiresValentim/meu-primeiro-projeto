97ede868f66fe71d41b81c07099bc39be3bde5db
TamiresValentim <tamiresvalentim@gmail.com>
2023-01-26 18:45:55 -0300

    meu primeiro projeto

---
+++ script.js
@@ -0,0 +1,30 @@
+<script>
+
+
+    let imagem = document.querySelector(".starbucks")
+    let circulo = document.querySelector(".circulo")
+
+
+    function trocaImagem(endereco) {
+        imagem.src = endereco
+    }
+
+    function trocaCor(cor){
+        circulo.style.background = cor
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</script>
